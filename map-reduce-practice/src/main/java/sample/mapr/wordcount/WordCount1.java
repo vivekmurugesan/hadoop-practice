83db3af4359b692c952368ce8261eacbc6a23586
Vivek Murugesan <vivek.murugesan@gmail.com>
2016-08-08 00:29:34 +0530

    Initial checkin.

---
+++ map-reduce-practice/src/main/java/sample/mapr/wordcount/WordCount1.java
@@ -0,0 +1,63 @@
+package sample.mapr.wordcount;
+
+import java.io.* ;
+import org.apache.hadoop.conf.Configuration ;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount1
+{
+	public static class WordCountMapper
+		extends Mapper<Object, Text, Text, IntWritable>
+		{
+			private final static IntWritable one = new IntWritable(1);
+			private Text word = new Text();
+			
+			public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException
+			{	
+				String words[] = value.toString().split(" ");
+				for(String str : words)
+				{	
+					word.set(str);
+					context.write(word, one);
+				}	
+			}
+		}
+	
+	public static class WordCountReducer
+		extends Reducer<Text, IntWritable, Text, IntWritable>
+		{
+			public void map(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException
+			{
+				int total = 0;
+				for(IntWritable val : values)
+				{
+					total++;
+				}
+				context.write(key, new IntWritable(total));
+			}
+		}
+	
+	public static void main(String[] args)throws IOException, InterruptedException, ClassNotFoundException
+	{
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "Word count");
+		job.setJarByClass(WordCount1.class);
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true)? 0 : 1);
+	}
+	
+}
